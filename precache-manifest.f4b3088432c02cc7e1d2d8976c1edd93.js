8405508f9ce851979d5e71dd15b64f7d53282eec
Turan Sultan <turan.sultann@gmail.com>
2020-08-03 00:56:55 +0600

    Updates

---
+++ precache-manifest.f4b3088432c02cc7e1d2d8976c1edd93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34c89546345620332bb9ac6120ebc25",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "eecfd4fa664e70e717f6",
+    "url": "/react-blog/static/css/main.27a70295.chunk.css"
+  },
+  {
+    "revision": "d53423cbd4f11601535f",
+    "url": "/react-blog/static/js/2.65cf600c.chunk.js"
+  },
+  {
+    "revision": "e3f5a7fe5db43d316e150df67470b9ce",
+    "url": "/react-blog/static/js/2.65cf600c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eecfd4fa664e70e717f6",
+    "url": "/react-blog/static/js/main.eba761b3.chunk.js"
+  },
+  {
+    "revision": "572581113006f46e799b",
+    "url": "/react-blog/static/js/runtime-main.d1b56ccc.js"
+  }
+]);
